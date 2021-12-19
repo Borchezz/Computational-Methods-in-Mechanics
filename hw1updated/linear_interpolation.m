1ad732fcc3af1a5ba90ba83a47971f49a66930ec
Anton Matsuganov <matsvr71@gmail.com>
2021-12-19 20:31:16 +0200

    2.15 updated

--- hw1updated/linear_interpolation.m
+++ hw1updated/linear_interpolation.m
@@ -1,28 +1,29 @@
+%Exercise 2.15: Linear interpolation
+
 function linear_interpolation()
-    N = 4;
-    delta_t = 1;
-    y = zeros(5,1);
-    y(1) = 4.4; 
-    y(2) = 2.0; 
-    y(3) = 11.0; 
-    y(4) = 21.5; 
-    y(5) = 7.5;
+format short 
+yfind()
 
-    find_y(y,N,delta_t);
 end
 
-function result = interpolate(y, t, delta_t)
-    % Uses linear interpolation to find intermediate y
-    i = floor(t) + 1;
-    % Scheme: y(t) = y_i + delta-y/delta-t * dt
-    result = y(i) + ((y(i+1) - y(i))/delta_t)*(t-floor(t));
+function interpl = interpolation(y, t) 
+i = floor(t)+1; 
+delta_t = 1; %step between times t
+interpl = y(i) + ((y(i + 1) - y(i))/delta_t)*(t-floor(t));
 end
-    
-function find_y(y, N, delta_t)
-    fprintf('For time t on the interval [0,%d]... \n', N);
-    t = input('Give your desired t: ');
-    while t >= 0
-        fprintf('y(t) = %g \n', interpolate(y, t, delta_t));
-        t = input('Give new time t: ');
-    end
+
+function yfind() %for task b and c, finds y at t by interpolation
+t = input('Please enter time value (t>0): '); 
+
+y = zeros(1,5);%values from task c
+y(1) = 4.4;
+y(2) = 2.0;
+y(3) = 11.0;
+y(4) = 21.5;
+y(5) = 7.5;
+
+while t >= 0 %The loop is terminated when the user gives a negative time
+    interpolation(y, t)
+    t = input('Please enter NEW time value (t>0): ');
+end
 end
